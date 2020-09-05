d6d5da040db1f5c510a1f37dce17fcd718d71af7
Michael Paddon <michael@paddon.org>
2020-09-05 19:17:12 +0900

    Release 0.7.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name = "imaplar",
-    version = "0.6.3",
+    version = "0.7",
     author = "Michael Paddon",
     author_email = "michael@paddon.org",
     description = "IMAP mailbox monitor",
