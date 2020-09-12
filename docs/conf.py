514bb42f6b27e2cad09ab2a5724ddd7f5a4f4ce5
Michael Paddon <michael@paddon.org>
2020-09-12 19:54:47 +0900

    Release 0.8.

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,9 @@ copyright = '2020, Michael Paddon'
 author = 'Michael Paddon'
 
 # The short X.Y version
-version = ''
+version = '0.8'
 # The full version, including alpha/beta/rc tags
-release = '0.7'
+release = '0.8'
 
 
 # -- General configuration ---------------------------------------------------
